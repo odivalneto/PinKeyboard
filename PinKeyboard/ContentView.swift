3f9d08ec5dbf9610343b71a36d087b019ac3b41b
Odival Neto <odivalneto@gmail.com>
2024-10-07 20:20:20 -0300

    Initial Commit

---
+++ PinKeyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinKeyboard
+//
+//  Created by Odival Neto on 07/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PinKeyboard/PinKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinKeyboardApp.swift
+//  PinKeyboard
+//
+//  Created by Odival Neto on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PinKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
